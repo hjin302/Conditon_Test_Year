a371a1c125ab4d9067fb5ff75cacef7b4a2b5f5e
hjin302 <enro302@naver.com>
2020-03-06 10:35:03 +0900

    년도 입력 가능
    -스캐너를 입력

---
+++ src/code/MainDrive.java
@@ -0,0 +1,17 @@
+package code;
+
+import java.util.Scanner;
+
+public class MainDrive {
+
+	// 새 한글은 잘됩니다.
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("궁금한 년도 입력 : ");
+		int year = sc.nextInt();
+		
+		System.out.println(year);
+	}
+
+}
